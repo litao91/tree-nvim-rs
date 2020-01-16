34cd16628a7bd7a3a0fcc74429d9a8d331c27205
LI Tao <litao.91@bytedance.com>
2020-01-16 15:47:55 +0800

    Optimize lock

--- src/tree_handler.rs
+++ src/tree_handler.rs
@@ -16,7 +16,7 @@ pub struct TreeHandlerData {
     treebufs: Vec<(i8, Vec<u8>)>, // recently used order
     // buffer: Option<Buffer<<TreeHandler as Handler>::Writer>>,
     buf_count: u32,
-    ctx: Context,
+    prev_bufnr: Option<(i8, Vec<u8>)>,
 }
 
 type TreeHandlerDataPtr = Arc<RwLock<TreeHandlerData>>;
@@ -55,7 +55,6 @@ impl<W: AsyncWrite + Send + Sync + Unpin + 'static> TreeHandler<W> {
         let bufnr = (bufnr.0, Vec::from(bufnr.1));
         let mut tree = Tree::new(bufnr.clone(), &buf, &nvim, ns_id).await?;
         {
-            let d = data.read().await;
             tree.config.update(&cfg_map)?;
         }
         tree.change_root(path, &nvim).await?;
@@ -65,7 +64,7 @@ impl<W: AsyncWrite + Send + Sync + Unpin + 'static> TreeHandler<W> {
             let mut d = data.write().await;
             d.trees.insert(bufnr.clone(), tree);
             d.treebufs.push(bufnr.clone());
-            d.ctx.prev_bufnr = Some(bufnr.clone());
+            d.prev_bufnr = Some(bufnr.clone());
         }
         nvim.execute_lua("resume(...)", vec![Value::Ext(bufnr.0, bufnr.1), tree_cfg])
             .await?;
@@ -78,10 +77,12 @@ impl<W: AsyncWrite + Send + Sync + Unpin + 'static> TreeHandler<W> {
     ) -> Result<Buffer<<Self as Handler>::Writer>, Box<dyn std::error::Error>> {
         let buf = nvim.create_buf(false, true).await.unwrap();
         info!("new buf created: {:?}", buf.get_value());
-        let mut d = data.write().await;
-        let buf_num = d.buf_count;
-        // TODO: use atomic?
-        d.buf_count += 1;
+        let buf_num;
+        {
+            let mut d = data.write().await;
+            buf_num = d.buf_count;
+            d.buf_count += 1;
+        }
         let buf_name = format!("Tree-{}", buf_num);
         buf.set_name(&buf_name).await?;
         Ok(buf)
@@ -113,29 +114,33 @@ impl<W: AsyncWrite + Send + Sync + Unpin + 'static> TreeHandler<W> {
             let buf = Self::create_buf(data.clone(), nvim.clone()).await?;
             Self::create_tree(data, nvim, buf, ns_id, &path, cfg_map).await?;
         } else {
-            let mut d = data.write().await;
-            // only a few items, wouldn't be a problem
-            let prev_bufnr = match &d.ctx.prev_bufnr {
-                Some(nr) => nr,
-                None => return Err(Box::new(ArgError::new("prev_bufnr not defined"))),
+            let bufnr_vals;
+            let tree_cfg;
+            {
+                let mut d = data.write().await;
+                // only a few items, wouldn't be a problem
+                let prev_bufnr = match &d.prev_bufnr {
+                    Some(nr) => nr,
+                    None => return Err(Box::new(ArgError::new("prev_bufnr not defined"))),
+                }
+                .clone();
+                let tree = match d.trees.get_mut(&prev_bufnr) {
+                    Some(t) => t,
+                    None => return Err(Box::new(ArgError::new("unknown tree"))),
+                };
+                tree.config.update(&cfg_map)?;
+                tree_cfg = tree.config.get_cfg_map();
+                d.treebufs.retain(|v| v != &prev_bufnr);
+                d.treebufs.push(prev_bufnr);
+                bufnr_vals = Value::Array(
+                    d.treebufs
+                        .iter()
+                        .rev()
+                        .cloned()
+                        .map(|v| Value::Ext(v.0, v.1))
+                        .collect(),
+                );
             }
-            .clone();
-            let tree = match d.trees.get_mut(&prev_bufnr) {
-                Some(t) => t,
-                None => return Err(Box::new(ArgError::new("unknown tree"))),
-            };
-            tree.config.update(&cfg_map)?;
-            let tree_cfg = tree.config.get_cfg_map();
-            d.treebufs.retain(|v| v != &prev_bufnr);
-            d.treebufs.push(prev_bufnr);
-            let bufnr_vals: Value = Value::Array(
-                d.treebufs
-                    .iter()
-                    .rev()
-                    .cloned()
-                    .map(|v| Value::Ext(v.0, v.1))
-                    .collect(),
-            );
             nvim.command("lua require('tree')").await?;
             nvim.execute_lua("resume(...)", vec![bufnr_vals, tree_cfg])
                 .await?;
@@ -253,6 +258,8 @@ impl<W: AsyncWrite + Send + Sync + Unpin + 'static> Handler for TreeHandler<W> {
                 error!("Arg num should be 3 but got {}", vl.len());
             }
 
+            let mut ctx = Context::default();
+
             // 3rd update context
             match vl.pop().unwrap() {
                 Value::Map(context_val) => {
@@ -264,8 +271,7 @@ impl<W: AsyncWrite + Send + Sync + Unpin + 'static> Handler for TreeHandler<W> {
                                 return;
                             }
                         };
-                        let mut d = self.data.write().await;
-                        d.ctx.update(&key, v);
+                        ctx.update(&key, v);
                     }
                 }
                 _ => {
@@ -286,11 +292,13 @@ impl<W: AsyncWrite + Send + Sync + Unpin + 'static> Handler for TreeHandler<W> {
 
             info!("async action: {}", action);
 
-            let mut d = self.data.write().await;
-            let ctx = d.ctx.clone();
-            if let Some(bufnr) = d.ctx.prev_bufnr.clone() {
-                if let Some(tree) = d.trees.get_mut(&bufnr) {
-                    tree.action(&neovim, &action, act_args, ctx).await;
+            {
+                let mut d = self.data.write().await;
+                d.prev_bufnr = ctx.prev_bufnr.clone();
+                if let Some(bufnr) = ctx.prev_bufnr.clone() {
+                    if let Some(tree) = d.trees.get_mut(&bufnr) {
+                        tree.action(&neovim, &action, act_args, ctx).await;
+                    }
                 }
             }
         }
